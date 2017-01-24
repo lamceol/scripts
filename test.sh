3417a900583d30532159a387f9d179a9b66140fb
lamceol <robbielambe@gmail.com>
2017-01-25 10:54:56 +1100

    Updates commands

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
-
-
-
-for (( i=1; i<=5; i++ ))
+#!/bin/sh
+#
+for i in (seq 1 5)
 do  
-   ifconfig | grep 'ether' >> rob.txt
-   ifconfig | grep "mtu"  >> rob.txt
+   dsl_pipe G997_LineIntegralStatusGet 0 0 | grep 'SNR' >> SNR.txt
+   dsl_pipe G997_LineIntegralStatusGet 0 1 | grep 'SNR' >> SNR.txt
+   dsl_pipe g997csg 0 0 0 | grep 'ActualNetDataRate' >> SNR.txt
+   dsl_pipe g997csg 0 0 1 | grep 'ActualNetDataRate' >> SNR.txt
    echo "Done iteration $i"
    sleep 2
 done
